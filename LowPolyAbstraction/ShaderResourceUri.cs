c0f568ada01c98b7d96a68fc12998f90cc729eb9
routersys <github.routersys@gmail.com>
2026-09-23 15:16:26 +0900

    シェーダーの生成をLowPolyAbstraction.Shaderへ切り出す

--- LowPolyAbstraction/ShaderResourceUri.cs
+++ LowPolyAbstraction/ShaderResourceUri.cs
@@ -2,5 +2,5 @@ namespace LowPolyAbstraction;
 
 internal static class ShaderResourceUri
 {
-    public static Uri Get(string shaderName) => new($"pack://application:,,,/LowPolyAbstraction;component/Shaders/{shaderName}.cso", UriKind.Absolute);
+    public static Uri Get(string shaderName) => new($"pack://application:,,,/LowPolyAbstraction;component/Resources/Shader/{shaderName}.cso", UriKind.Absolute);
 }
