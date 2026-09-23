0d2b7f95698fdd3da6e947ef8106674716c82e82
routersys <github.routersys@gmail.com>
2026-09-23 18:37:13 +0900

    更新確認をWPF起動中に限り行う

--- LowPolyAbstraction/LowPolyAbstractionUpdateNotifier.cs
+++ LowPolyAbstraction/LowPolyAbstractionUpdateNotifier.cs
@@ -13,7 +13,7 @@ internal static class LowPolyAbstractionUpdateNotifier
 
     public static void EnsureCheckedOnce()
     {
-        if (Interlocked.Exchange(ref _started, 1) != 0)
+        if (Application.Current is null || Interlocked.Exchange(ref _started, 1) != 0)
             return;
         _ = RunAsync();
     }
